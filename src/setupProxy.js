dd8dcc52f9ecdb3d8a67b53b488e6384053d055b
Liran Yin <liranyin@Lirans-MacBook-Pro.local>
2019-12-10 21:24:19 -0500

    fourth commit

--- src/setupProxy.js
+++ src/setupProxy.js
@@ -1,6 +1,6 @@
-const proxy = require("http-proxy-middleware");
+import proxy from "http-proxy-middleware";
 
-module.exports = function(app) {
+export default function(app) {
     app.use(
         proxy("/users", {
             target: "https://api.darksky.net",
